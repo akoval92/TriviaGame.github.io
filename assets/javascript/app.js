89e2aaa6cc4a3df730a8f6181e472fec24be0395
Amber Koval <amberkoval92@gmail.com>
2019-03-23 01:26:48 -0700

    add the nonsense

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,118 @@ var interval = setInterval(function() {
 
     if (time <= 0) {
      		clearInterval(time);
-      	$('#timer').html("00:60");  
+      	$('#timer').text("60");    // something here!!!!!!!!!!!!!!!!!!!!!!!
         return;
     }else{
     	$('#time').text(interval);
-      console.log("Time --> " + time);
+
+      $("#timer").text(time);
     }
 }, 1000);
 
 }
+// ***************************************************************************************************************************************
+// Need to create toggle for each set of Qs and As
+
+
+// ************original code\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+
+// var cor = 0;
+// var wro = 0;
+
+
+// $("#whichkey").on("mousedown", function(event) {
+//     if ($("#right").text("Correct:" + event.which)); {
+//     }
+//         cor++;
+                                                                            // P*O*S*S*I*B*L*E*   1
+
+// $("#whichkey").on("mousedown", function(event) {
+//     if ($("#wrong").text("Incorrect:" + event.which)); {
+//     }
+//         wro++; 
+// }
+
+// \/\/\/\/\/\/\/\/original code^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+
+// \/\/\/\/\/\/\/\/lightbulb change\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+
+select();
+    function select() {
+        var cor = 0;
+        var wro = 0;
+        $("#whichkey").on("mousedown", function (event) {
+            if ($("#right").text("Correct:" + event.which))
+                ;
+            {
+                                                                                            // P*O*S*S*I*B*L*E*   2
+
+            }
+            cor++;
+            $("#whichkey").on("mousedown", function (event) {
+                if ($("#wrong").text("Incorrect:" + event.which))
+                    ;
+                {
+                }
+                wro++;
+            });
+        });
+    }
+
+// *********************lightbulb change **************^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+($(".button").on('click', score)); {
+
+function score() {
+
+    if ($(".button").click("#rightansw")); {
+        cor++;
+    }
+                                                                                // P*O*S*S*I*B*L*E*   3
+
+
+    if ($(".button").click("#wrongansw")); {                /* why doesn't 'else' work?? */
+        wro++;
+    }
+}
+}
+
+
+
+// \/\/\/\/\/\/\/\/\/\/\/\will .click work better than .hasClass here?? \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+
+
+
+function score() {
+
+    if ($(".button").hasClass("#rightansw")); {
+        cor++;
+    }
+                                                                            // P*O*S*S*I*B*L*E*   4
+
+    if ($(".button").hasClass("#wrongansw")); {                
+        wro++;
+    }
+}
+
+// \/\/\/\/\/\/\/ Function to tell user question has already been anwered\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+if ($("#.button").is(score)) {
+    alert("Answer already selected, next question");
+}
+
+
+// **********************NOTES******************************************
+
+// * Are any of my code creations possibly working?
+// * Where is the 2/60 coming from?
+
