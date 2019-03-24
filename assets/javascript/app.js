f697355504b0daa490344f6a4a3c7c6d1e514d7b
Amber Koval <amberkoval92@gmail.com>
2019-03-23 21:51:20 -0700

    push you are a failure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,126 +1,44 @@
+
+
 $(".btnStart").on("click", start);
 
 function start() {
-    
 
-var time = 60;
-var interval = setInterval(function() {
-    time--;
+    var time = 60;
+    setInterval(function () {
+        time--;
 
-    if (time <= 0) {
-     		clearInterval(time);
-      	$('#timer').text("60");    // something here!!!!!!!!!!!!!!!!!!!!!!!
-        return;
-    }else{
-    	$('#time').text(interval);
-
-      $("#timer").text(time);
-    }
-}, 1000);
+        if (time <= 0) {
+            clearInterval(time);
+            $('#time').text("60");
+            return;
+        } else {
+            $('#time').text(time);
+        }
+    }, 1000);
 
 }
-// ***************************************************************************************************************************************
-// Need to create toggle for each set of Qs and As
 
 
-// ************original code\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+$(".btn").on("click", score);
 
-
-// var cor = 0;
-// var wro = 0;
-
-
-// $("#whichkey").on("mousedown", function(event) {
-//     if ($("#right").text("Correct:" + event.which)); {
-//     }
-//         cor++;
-                                                                            // P*O*S*S*I*B*L*E*   1
-
-// $("#whichkey").on("mousedown", function(event) {
-//     if ($("#wrong").text("Incorrect:" + event.which)); {
-//     }
-//         wro++; 
-// }
-
-// \/\/\/\/\/\/\/\/original code^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-
-
-// \/\/\/\/\/\/\/\/lightbulb change\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
-
-select();
-    function select() {
-        var cor = 0;
-        var wro = 0;
-        $("#whichkey").on("mousedown", function (event) {
-            if ($("#right").text("Correct:" + event.which))
-                ;
-            {
-                                                                                            // P*O*S*S*I*B*L*E*   2
-
-            }
-            cor++;
-            $("#whichkey").on("mousedown", function (event) {
-                if ($("#wrong").text("Incorrect:" + event.which))
-                    ;
-                {
-                }
-                wro++;
-            });
-        });
-    }
-
-// *********************lightbulb change **************^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-
-($(".button").on('click', score)); {
+var cor = 0;
+var wro = 0;
 
 function score() {
 
-    if ($(".button").click("#rightansw")); {
+   
+
+    if ($(this).hasClass("rightansw")); {
         cor++;
+        // $("#cor").text("Correct:" + cor)
+        console.log("Correct:" + cor)
+
     }
-                                                                                // P*O*S*S*I*B*L*E*   3
-
-
-    if ($(".button").click("#wrongansw")); {                /* why doesn't 'else' work?? */
+    if ($(this).hasClass("wrongansw")); {
         wro++;
-    }
-}
-}
-
-
-
-// \/\/\/\/\/\/\/\/\/\/\/\will .click work better than .hasClass here?? \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
-
-
-
-function score() {
-
-    if ($(".button").hasClass("#rightansw")); {
-        cor++;
-    }
-                                                                            // P*O*S*S*I*B*L*E*   4
-
-    if ($(".button").hasClass("#wrongansw")); {                
-        wro++;
-    }
+        // $("#wro").text("Inorrect:" + wro)    }
+        console.log("Inorrect:" + wro)    }
 }
 
-// \/\/\/\/\/\/\/ Function to tell user question has already been anwered\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-
-
-if ($("#.button").is(score)) {
-    alert("Answer already selected, next question");
-}
-
-
-// **********************NOTES******************************************
-
-// * Are any of my code creations possibly working?
-// * Where is the 2/60 coming from?
-
+$(".btn").on("click", highlight);
